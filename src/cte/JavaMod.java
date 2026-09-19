9e62c347a9e0177a3947e0b1f4101ce26d846d25
mzdsrn <3793380683@qq.com>
2026-09-19 17:40:28 +0800

    Added tech tree and made fields' names meaningful.

--- src/cte/JavaMod.java
+++ src/cte/JavaMod.java
@@ -3,6 +3,7 @@ package cte;
 import arc.util.*;
 import cte.contents.Blocks;
 import cte.contents.Units;
+import mindustry.content.TechTree;
 import mindustry.mod.*;
 import mindustry.world.meta.BuildVisibility;
 
@@ -18,5 +19,17 @@ public class JavaMod extends Mod{
         Units.load();
         Blocks.load();
         mindustry.content.Blocks.coreShard.buildVisibility = BuildVisibility.shown;
+        createTechTree();
+    }
+
+    public void createTechTree(){
+        TechTree.nodeRoot("cte", Blocks.poolBuilder, () ->{
+            TechTree.node(Blocks.floorPlacer, () ->{
+                TechTree.node(Blocks.coreZonePlacer);
+                TechTree.node(Blocks.rotatableFloorPlacer, () -> TechTree.node(Blocks.rotatableContinuableFloorPlacer));
+            });
+            TechTree.node(Blocks.oreBuilder);
+            TechTree.node(Blocks.wallBlaster);
+        });
     }
 }
--- src/cte/contents/Blocks.java
+++ src/cte/contents/Blocks.java
@@ -11,19 +11,19 @@ import static mindustry.type.ItemStack.with;
 
 public class Blocks {
     public static Block
-            block,
-            block2,
-            block3, block4, block6, block7,
-            block5
+            poolBuilder,
+            wallBlaster,
+            floorPlacer, coreZonePlacer, rotatableFloorPlacer, rotatableContinuableFloorPlacer,
+            oreBuilder
     ;
 
     public static void load(){
-        block = new PoolBuilder("pool-builder"){{
+        poolBuilder = new PoolBuilder("pool-builder"){{
             requirements(Category.liquid, with(Items.graphite, 50, Items.silicon, 2, Items.copper, 10));
             size = 1;
             liquidCapacity = 10;
         }};
-        block2 = new WallBlaster("wall-blaster"){{
+        wallBlaster = new WallBlaster("wall-blaster"){{
             setItem(Items.blastCompound, b -> Mathf.floor((float) b.items.get(Items.blastCompound) / 2));
             itemCapacity = 45;
             max = Mathf.floor((float) itemCapacity / 2);
@@ -31,30 +31,30 @@ public class Blocks {
             health = 60;
             requirements(Category.effect, ItemStack.with(Items.copper, 80, Items.lead, 50));
         }};
-        block3 = new FloorPlacer("floor-placer"){{
+        floorPlacer = new FloorPlacer("floor-placer"){{
             size = 1;
             health = 60;
             requirements(Category.effect, ItemStack.with(Items.copper, 130, Items.lead, 20));
             recordOverlay = true;
         }};
-        block4 = new FloorPlacer("core-zone-placer") {{
+        coreZonePlacer = new FloorPlacer("core-zone-placer") {{
             size = 1;
             health = 50;
             requirements(Category.effect, with(Items.copper, 80, Items.lead, 60, Items.silicon, 20));
             floor = mindustry.content.Blocks.coreZone.asFloor();
             consumesTime = consumesTime * 2.5f;
         }};
-        block5 = new OreBuilder("ore-builder"){{
+        oreBuilder = new OreBuilder("ore-builder"){{
             requirements(Category.crafting, with(Items.graphite, 80, Items.silicon, 10, Items.copper, 15));
             size = 1;
             itemCapacity = 20;
         }};
-        block6 = new RotatableFloorPlacer("rotatable-floor-placer"){{
+        rotatableFloorPlacer = new RotatableFloorPlacer("rotatable-floor-placer"){{
             size = 1;
             health = 60;
             requirements(Category.effect, ItemStack.with(Items.copper, 130, Items.lead, 20));
         }};
-        block7 = new RotatableContinuableFloorPlacer("rotatable-continuable-floor-placer"){{
+        rotatableContinuableFloorPlacer = new RotatableContinuableFloorPlacer("rotatable-continuable-floor-placer"){{
             size = 1;
             health = 60;
             consumesTime = 60;
